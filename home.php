2812c8a422a2a292a94e08bf1664615058345aee
Frederick Adarode <109428219+Rickofar@users.noreply.github.com>
2022-07-19 18:50:34 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,128 @@
+<!DOCTYPE HTML>
+<html>
+
+	<head>
+	
+		<title>Radiant Amore | Essence of Beauty</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-witdth, initial-scale=1">
+		<link href="style.css" type="text/css" rel="stylesheet">
+	
+	<style>
+
+   
+   </style>
+		
+	</head>
+	
+	 <body>
+	 
+	 	<!-- header -->
+		<div class="header">
+		<img class="logo" src="logo1.png">
+		
+     <div id="wrap">
+      <ul class="navbar">
+        <li>
+          <a href="home.php">Home</a>
+        </li>
+        <li>
+          <a href="productscategories.php">Products</a>
+          <ul>
+            <li>
+              <a href="skincare.php">Skin Care</a>
+            </li>
+            <li>
+              <a href="haircare.php">Hair Care</a>
+            </li>
+            <li>
+              <a href="hairextensions.php">Hair Extensions</a>
+            </li>
+            <li>
+              <a href="smellnice.php">Smell Nice</a>
+            </li>
+			<li>
+              <a href="accessories.php">Accessories</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="services.php">Services</a>
+          <ul>
+		     <li>
+              <a href="appointment.php">Appointment</a>
+            </li>
+		   </ul>
+        <li>
+          <a href="aboutus.php">About Us</a>
+          <ul>
+            <li>
+              <a href="ourstory.php">Our Story</a>
+            </li>
+            <li>
+              <a href="ourmission.php">Our Mission</a>
+            </li>
+            <li>
+              <a href="ourstaff.php">Our Staff</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="contactus.php">Constact Us</a>
+        </li>
+		<li> 
+			<a class="addtocart"><a href="addtocart.php"><b><i>Cart</b></i></a></a>
+		</li>
+		<li>
+          <a href="account.php">Account</a>
+          <ul>
+            <li>
+              <a href="login.php">Login</a>
+            </li>
+            <li>
+              <a href="signup.php">Signup</a>
+            </li>
+            <li>
+              <a href="profile.php">Profile</a>
+            </li>
+          </ul>
+        </li>
+			<div class="wrap">
+   <div class="search">
+      <input type="text" class="searchTerm" placeholder="What are you looking for?">
+      <button type="submit" class="searchButton">
+        <i class="fa fa-search"></i>
+     </button>
+   </div>
+</div>
+		</div>
+	  </ul>
+    </div>
+	
+<div id="captioned-gallery">
+	<figure class="slider">
+		<figure>
+			<img src="slidepic01.png" alt>
+			<figcaption></figcaption>
+		</figure>
+		<figure>
+			<img src="slidepic02.png" alt>
+			<figcaption></figcaption>
+		</figure>
+		<figure>
+			<img src="slidepic03.png" alt>
+			<figcaption></figcaption>
+		</figure>
+		<figure>
+			<img src="slidepic04.png" alt>
+			<figcaption></figcaption>
+		</figure>
+	</figure>
+</div>
+
+
+
+
+	
+  </body>
+</html>
